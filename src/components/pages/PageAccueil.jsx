7ee80866eb8366b955059bcb4cf42062f4398df5
ali-bennezia <bennezia.ali@gmail.com>
2022-07-05 22:28:16 +0200

    Routage, premières mises en place pages

---
+++ src/components/pages/PageAccueil.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import "./PagesCommon.css";
+
+function PageAccueil() {
+  useEffect(() => {
+    console.log("test");
+  }, []);
+
+  return <div></div>;
+}
+
+export default PageAccueil;
