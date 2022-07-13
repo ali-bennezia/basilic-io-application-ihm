04d88175f72709303bb189945e8b5aa5ac2196ed
ali-bennezia <bennezia.ali@gmail.com>
2022-07-13 22:46:41 +0200

    Ajout d'illustrations

--- src/components/pages/PageAccueil.jsx
+++ src/components/pages/PageAccueil.jsx
@@ -142,15 +142,8 @@ function PageAccueil() {
           <h4 className="lower-main-subtitle-h4">
             Quelques unes des technologies employées
           </h4>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-around",
-                alignItems: "center",
-                width: "50%",
-              }}
-            >
+          <div className="horizontal-layout-outer">
+            <div className="horizontal-layout-inner">
               <img src="/img/logos/expressJS_logo.png" height="175" />
               <img src="/img/logos/mongodb_logo.png" height="75" />
               <img src="/img/logos/nodejs_logo.png" height="150" />
@@ -160,10 +153,16 @@ function PageAccueil() {
           <h1 className="lower-main-subtitle-h1">Comment ?</h1>
 
           <p className="lower-main-p">
-            Le projet est organisé selon le principe d'architecture n-tiers.
+            Le projet est organisé selon les principes d'architecture n-tiers et
+            MVC. Par ailleurs, il est divisé en plusieurs microservices répartis
+            sur différentes couches.
             <br />
-            <h4 className="lower-main-subtitle-h5">La couche IHM</h4>
+            La communication entre tout ces microservices se fait principalement
+            par le biais d'APIs REST.
             <br />
+          </p>
+          <h5 className="lower-main-subtitle-h5">La couche IHM</h5>
+          <p className="lower-main-p">
             La première couche, la plus superficielle, est la couche IHM. Elle
             est basée sur React et permet aux utilisateurs d'intéragir avec le
             réseau.
@@ -171,9 +170,56 @@ function PageAccueil() {
             Elle concerne la partie la plus visible du projet. Elle communique
             par ailleurs avec la seconde couche, la couche métier.
             <br />
-            <h4 className="lower-main-subtitle-h5">La couche métier</h4>
-            <br />
           </p>
+          <h5 className="lower-main-subtitle-h5">La couche métier</h5>
+          <p className="lower-main-p">
+            La couche métier est celle sur laquelle se déroulent la plupart des
+            traitements. C'est notamment sur cette couche que se trouve
+            <br />
+            en grande majorité toute la logique qui régis le réseau. Elle est la
+            couche principale du projet et celle qui a demandé le plus de temps
+            de développement.
+            <br />
+            Elle fonctionne sur NodeJS, conjointement avec le framework Express.{" "}
+            <br />
+            Par ailleurs, la communication entre les différentes couches se fait
+            principalement par le biais d'APIs REST.
+          </p>
+          <h5 className="lower-main-subtitle-h5">La couche de données</h5>
+          <p className="lower-main-p">
+            La couche la plus profonde, la couche de données, contient la base
+            de donnée ainsi que les médias.
+            <br />
+            C'est donc sur cette couche que sont stoquées toutes les
+            informations et tout le contenu du réseau, ce qui inclut aussi bien
+            les membres que des photos, des vidéos, des posts.
+            <br />
+            Sur cette couche se trouvent deux serveurs: l'un gérant la base de
+            donnée, l'autre gérant les médias.
+          </p>
+          <h5 className="lower-main-subtitle-h5">Illustration</h5>
+          <div className="horizontal-layout-outer">
+            <div
+              className="horizontal-layout-inner"
+              style={{
+                padding: "40px",
+                width: "38%",
+              }}
+            >
+              <div className="circle-server-illustration"></div>
+              <div className="circle-server-illustration"></div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                }}
+              >
+                <div className="circle-server-illustration"></div>
+                <div className="circle-server-illustration"></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
