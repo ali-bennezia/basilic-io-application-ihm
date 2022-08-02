65a74e11c9a14b63cc391853a9f7b836be894bf1
ali-bennezia <bennezia.ali@gmail.com>
2022-08-02 22:32:03 +0200

    Ajout onglets paramètres

--- src/components/pages/PageParams.jsx
+++ src/components/pages/PageParams.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import BasePage from "./commun/BasePage";
+
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 import "./commun/PagesCommun.css";
 
 function PageParams() {
+  const [tabIndex, setTabIndex] = useState(0);
+
   return (
     <BasePage
       style={{
@@ -14,6 +19,24 @@ function PageParams() {
       <h1 className="page-title" style={{ marginTop: "26px" }}>
         Paramètres
       </h1>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Tabs
+          TabIndicatorProps={{
+            style: {
+              background: "#3bd34d",
+              backgroundColor: "#3bd34d",
+              color: "#3bd34d",
+            },
+          }}
+          indicatorColor={"#3bd34d"}
+          value={tabIndex}
+          onChange={(event, newValue) => setTabIndex(newValue)}
+        >
+          <Tab label="Profil public" value={0} />
+          <Tab label="Compte d'utilisateur" value={1} />
+          <Tab label="Supprimer" value={2} />
+        </Tabs>
+      </div>
     </BasePage>
   );
 }
